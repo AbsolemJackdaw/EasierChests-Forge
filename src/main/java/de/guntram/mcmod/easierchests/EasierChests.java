fe7b49eefed5a9afa0d0ed24d999189778c92743
Subaraki <robbeenaxel@hotmail.com>
2022-06-07 00:09:32 +0200

    open up to servers

--- src/main/java/de/guntram/mcmod/easierchests/EasierChests.java
+++ src/main/java/de/guntram/mcmod/easierchests/EasierChests.java
@@ -5,6 +5,8 @@ import net.minecraft.client.KeyMapping;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -25,31 +27,16 @@ public class EasierChests {
     public static KeyMapping keySortChest, keyMoveToChest,
             keySortPlInv, keyMoveToPlInv,
             keySearchBox;
-
-    private static HashMap<String, ChestGuiInfo> modHelpers = new HashMap<>();
     public static Logger LOGGER = LogManager.getLogger(EasierChests.class);
+    private static HashMap<String, ChestGuiInfo> modHelpers = new HashMap<>();
 
     public EasierChests() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+                () -> new IExtensionPoint.DisplayTest(() -> "useClientOnly", (ver, remote) -> true));
+
         final IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
         modBus.addListener(this::clientInit);
 
-    }
-
-    public void clientInit(FMLClientSetupEvent clientSetupEvent) {
-//        CrowdinTranslate.downloadTranslations(MODID);
-        ConfigurationHandler.getInstance();
-
-        FrozenSlotDatabase.init(new File("config"));
-
-        keySortChest = registerKey("sortchest", GLFW.GLFW_KEY_KP_7);
-        keyMoveToChest = registerKey("matchup", GLFW.GLFW_KEY_KP_8);
-        keySortPlInv = registerKey("sortplayer", GLFW.GLFW_KEY_KP_1);
-        keyMoveToPlInv = registerKey("matchdown", GLFW.GLFW_KEY_KP_2);
-        keySearchBox = registerKey("searchbox", GLFW.GLFW_KEY_UNKNOWN);
-
-        registerMod("inmis", "draylar.inmis.ui.BackpackScreenHandler", "de.guntram.mcmod.easierchests.storagemodapi.InmisHelper");
-        registerMod("Reinforced", "atonkish.reinfcore.screen.ReinforcedStorageScreenHandler", "de.guntram.mcmod.easierchests.storagemodapi.ReinforcedHelper");
-        registerMod("Expanded Storage", "ninjaphenix.container_library.api.inventory.AbstractHandler", "de.guntram.mcmod.easierchests.storagemodapi.ExpandedStorageHelper");
     }
 
     public static void registerMod(String screenHandlerClassName, ChestGuiInfo helper) {
@@ -71,6 +58,22 @@ public class EasierChests {
         return modHelpers.get(handler.getClass().getCanonicalName());
     }
 
+    public void clientInit(FMLClientSetupEvent clientSetupEvent) {
+//        CrowdinTranslate.downloadTranslations(MODID);
+        ConfigurationHandler.getInstance();
+
+        FrozenSlotDatabase.init(new File("config"));
+
+        keySortChest = registerKey("sortchest", GLFW.GLFW_KEY_KP_7);
+        keyMoveToChest = registerKey("matchup", GLFW.GLFW_KEY_KP_8);
+        keySortPlInv = registerKey("sortplayer", GLFW.GLFW_KEY_KP_1);
+        keyMoveToPlInv = registerKey("matchdown", GLFW.GLFW_KEY_KP_2);
+        keySearchBox = registerKey("searchbox", GLFW.GLFW_KEY_UNKNOWN);
+
+        registerMod("inmis", "draylar.inmis.ui.BackpackScreenHandler", "de.guntram.mcmod.easierchests.storagemodapi.InmisHelper");
+        registerMod("Reinforced", "atonkish.reinfcore.screen.ReinforcedStorageScreenHandler", "de.guntram.mcmod.easierchests.storagemodapi.ReinforcedHelper");
+        registerMod("Expanded Storage", "ninjaphenix.container_library.api.inventory.AbstractHandler", "de.guntram.mcmod.easierchests.storagemodapi.ExpandedStorageHelper");
+    }
 
     private KeyMapping registerKey(String key, int code) {
         KeyMapping result = new KeyMapping("key.easierchests." + key, code, category);
