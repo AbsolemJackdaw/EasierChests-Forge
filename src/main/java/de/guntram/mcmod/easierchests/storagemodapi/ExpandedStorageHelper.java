15fecc5c7ff75f4617d8fab77dbd128f29aff6c8
Subaraki <robbeenaxel@hotmail.com>
2022-06-07 00:10:00 +0200

    reimplement rudimentary support for expanded chests untill their API has exposed data

--- src/main/java/de/guntram/mcmod/easierchests/storagemodapi/ExpandedStorageHelper.java
+++ src/main/java/de/guntram/mcmod/easierchests/storagemodapi/ExpandedStorageHelper.java
@@ -1,36 +1,45 @@
-//package de.guntram.mcmod.easierchests.storagemodapi;
-//
-//import net.minecraft.world.inventory.AbstractContainerMenu;
-//import ninjaphenix.container_library.api.inventory.AbstractHandler;
-//import org.apache.logging.log4j.LogManager;
-//
-//public class ExpandedStorageHelper implements ChestGuiInfo {
-//
-//    @Override
-//    public int getRows(AbstractContainerMenu handler) {
-//        try {
-//            return ((AbstractHandler) handler).getMenuHeight();
-//        } catch (NoSuchMethodError ex) {
-//            warnOutdated("Reinforced");
-//            return -1;
-//        }
-//    }
-//
-//    @Override
-//    public int getColumns(AbstractContainerMenu handler) {
-//        try {
-//            return ((AbstractHandler) handler).getMenuWidth();
-//        } catch (NoSuchMethodError ex) {
-//            warnOutdated("Reinforced");
-//            return -1;
-//        }
-//    }
-//
-//    private boolean warnedOutdated = false;
-//    private void warnOutdated(String what) {
-//        if (!warnedOutdated) {
-//            LogManager.getLogger(this.getClass()).warn("You need a current version of "+what+", trying to fall back");
-//            warnedOutdated = true;
-//        }
-//    }
-//}
+package de.guntram.mcmod.easierchests.storagemodapi;
+
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import ninjaphenix.container_library.api.client.gui.AbstractScreen;
+import org.apache.logging.log4j.LogManager;
+
+import java.lang.reflect.Field;
+
+public class ExpandedStorageHelper implements ChestGuiInfo {
+
+    private boolean warnedOutdated = false;
+
+    @Override
+    public int getRows(AbstractContainerScreen<?> screen) {
+        try {
+            Field myField = ((AbstractScreen) screen).getClass().getDeclaredField("inventoryHeight");
+            myField.setAccessible(true);
+            //((AbstractScreen) screen).inventoryHeight
+            return myField.getInt(((AbstractScreen) screen));
+        } catch (NoSuchMethodError | NoSuchFieldException | IllegalAccessException ex) {
+            warnOutdated("Reinforced");
+            return -1;
+        }
+    }
+
+    @Override
+    public int getColumns(AbstractContainerScreen<?> screen) {
+        try {
+            Field myField = ((AbstractScreen) screen).getClass().getDeclaredField("inventoryWidth");
+            myField.setAccessible(true);
+            //((AbstractScreen) screen).inventoryHeight
+            return myField.getInt(((AbstractScreen) screen));
+        } catch (NoSuchMethodError | NoSuchFieldException | IllegalAccessException ex) {
+            warnOutdated("Reinforced");
+            return -1;
+        }
+    }
+
+    private void warnOutdated(String what) {
+        if (!warnedOutdated) {
+            LogManager.getLogger(this.getClass()).warn("You need a current version of " + what + ", trying to fall back");
+            warnedOutdated = true;
+        }
+    }
+}
